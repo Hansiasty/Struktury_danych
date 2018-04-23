ceaf84d620ed3e0ec9307905d00831a630d46b9a
Bartłomiej Jóźwiak <bartek@stud-jozwiak-1.math.uni.lodz.pl>
2018-04-23 11:38:04 +0200

    usunzpar WIP

--- Cwiczenia9/Sznur.cpp
+++ Cwiczenia9/Sznur.cpp
@@ -2,4 +2,52 @@
 // Created by Bartłomiej Jóźwiak on 23.04.2018.
 //
 #include <iostream>
+#include "Sznur.h"
+
 using namespace std;
+
+Sznur::Sznur() {
+    pocz=NULL;
+    ilejest=0;
+}
+
+Sznur::~Sznur() {
+
+}
+void Sznur::wstaw(int _dane) {
+
+}
+
+void Sznur::usunFragmenty(int N) {}
+
+void Sznur::usunZPar(int G) {
+    if(pocz==NULL || ilejest==1){
+        return;
+    }
+    else{
+        Box* przedPara=NULL;
+        Box*pWP=pocz;
+        Box* dWP=pocz->nast;
+        int suma;
+        int licz1=pWP->dane;
+        int licz2=dWP->dane;
+        suma=licz1+licz2;
+        if(suma<G)
+        {
+            if(pWP==pocz)
+            {
+                pocz=dWP;
+                delete pWP;
+                dWP=dWP->nast;
+                pWP=pocz;
+            }
+            else{
+                
+            }
+        }
+
+
+
+    }
+
+}
